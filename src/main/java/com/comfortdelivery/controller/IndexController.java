817d0801569e72d4e5264e1c8785062aba6abe65
Roman <fvroman94@gmail.com>
2018-05-17 19:22:54 +0700

    #InitializeProject

---
+++ src/main/java/com/comfortdelivery/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.comfortdelivery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String home(ModelMap map) {
+        return "index";
+    }
+}
